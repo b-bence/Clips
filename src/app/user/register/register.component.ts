1734ae9fd4161e1ae0c780e774c48c502d6ee3e9
Bence <borsi92@gmail.com>
2022-01-20 15:46:49 +0100

    added a presentational component(input component)

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -10,16 +10,23 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  name = new FormControl('',[
+    Validators.required,
+    Validators.minLength(3)
+  ])
+  email = new FormControl('')
+  age = new FormControl('')
+  password = new FormControl('')
+  confirmPassword = new FormControl('')
+  phoneNumber = new FormControl('')
 
   registerForm = new FormGroup({ 
-    name: new FormControl('',[
-      Validators.required
-    ]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    phoneNumber: new FormControl('')
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    phoneNumber: this.phoneNumber
    })
 
 }
