6579b343d0981f5724d85132692dfe32782226f0
Bence <borsi92@gmail.com>
2022-02-02 08:15:58 +0100

    adding query parameter to manage page

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
+  // 2 means ascending, 1 means descening order
+  videoOrder = '1'
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params:Params) => {
+      this.videoOrder = params.sort === '2' ? params.sort : '1'
+      // We don't have to unsubscribe from this observable because Angular will complete it when the component is destroyed
+      // It is destroyed when the user navigates to a different page
+
+    })
+  }
+
+  sort(event: Event){
+    const {value} = (event.target as HTMLSelectElement)
+
+    this.router.navigateByUrl(`/manage?sort=${value}`)
   }
 
 }
