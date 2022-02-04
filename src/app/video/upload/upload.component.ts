8750b6290bfa2dd4f611799a2aeb4824207609be
Bence <borsi92@gmail.com>
2022-02-04 08:12:47 +0100

    handling file drop event specifically for Chrome

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
   isDragover = false
+  file: File | null = null
 
   constructor() { }
 
@@ -15,6 +16,22 @@ export class UploadComponent implements OnInit {
 
   storeFile($event: Event){
     this.isDragover = false
+
+    // To be able to log files which are dropped in Chrome we have to do an extra step
+    // dataTransfer --> files property is empty when dropped something
+    // Chrome does not log the files unless we access it directly
+
+    // Nullish coalescing operator in this case: the method could return a value of undefined, which is not good for our file variable
+    // We could extend and add undefined too, but the nullish coalescing operator can be a good solution too
+    // In case the value is File or null, it will return that (left side of the operator), 
+    // but if its undefined the the right side of the operator will be returned
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+
+
+    if (!this.file || this.file.type !== 'video/mp4'){
+      return
+    }
+    console.log(this.file)
   }
 
 }
