1de92d64f98d03989eb737c3317b9a5e80400cad
Bence <borsi92@gmail.com>
2022-03-09 08:26:09 +0100

    adding multiple screenshots

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -32,26 +32,37 @@ async getScreenshots(file: File){
   // First we have to write to FS (File System) to access files from there
   this.ffmpeg.FS("writeFile",file.name, data)
 
-  // We are not able to run ffmpeg on the command line and provide options. 
-  // It is not installed on our computer. Instead, we are working with a WebAssembly version of ffmpe
-  // The ffmpeg package can execute the commands
-  // Add options we would run on the command line
-  await this.ffmpeg.run(
+
+  // Generate 3 screenshots from the first 3 seconds
+  const seconds = [1,2,3]
+  const commands: string[] = []
+
+  seconds.forEach((second) => {
+    commands.push(
     // Run command is added by default at the beginning
 
      // Input: grab a specific file from the File System (FS)
-    '-i', file.name,
+     '-i', file.name,
 
      // Output Options
      //-ss: configure the current timestamp. By default it is set to the beginning of the video. Format: hh:mm:ss
-    '-ss','00:00:01',
+    '-ss',`00:00:0${second}`,
     // We refer to images as frames. '1' refers to how many frames we want to capture at the given timestamp
     '-frames:v','1',
     // Resizing the image. -1 will keep the original aspect ratio by calculating the height
     '-filter:v','scale=510:-1',
 
      // Output: name of the file
-     'output_01.png'
+     `output_0${second}.png`
+    )
+  })
+
+  // We are not able to run ffmpeg on the command line and provide options. 
+  // It is not installed on our computer. Instead, we are working with a WebAssembly version of ffmpe
+  // The ffmpeg package can execute the commands
+  // Add options we would run on the command line
+  await this.ffmpeg.run(
+    ...commands
   )
 }
 
