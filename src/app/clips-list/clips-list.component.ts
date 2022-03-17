6c8fe8ad3f14f23c8af2b2bfcf708b2e97a82e38
Bence <borsi92@gmail.com>
2022-03-17 08:22:59 +0100

    handling scroll events on main page

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
   styleUrls: ['./clips-list.component.css']
 })
-export class ClipsListComponent implements OnInit {
+export class ClipsListComponent implements OnInit,OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  ngOnDestroy(): void {
+      window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  // It has to be an arrow function, because with a normal function we would not be able to access component's injective services
+  handleScroll = () => {
+    const { scrollTop, offsetHeight } = document.documentElement
+    const { innerHeight } = window
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
+
+    if (bottomOfWindow){
+      console.log('Reached the bottom of the window')
+    }
   }
 
 }
