4dcb7e9ac0c4e3a63ccdc27e3cd6fc259937d9b7
Bence <borsi92@gmail.com>
2022-03-22 08:11:26 +0100

    using the ViewChild decorator to select video player element

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Params} from '@angular/router';
 
 @Component({
@@ -8,6 +8,9 @@ import { ActivatedRoute, Params} from '@angular/router';
 })
 export class ClipComponent implements OnInit {
   id = ''
+  // Will perform a query on the template -> can select components, directives and regular html elements
+  // Static: true -> the decorator will update this property with the element before ngOnInit function is called -> can initialize it in ngOnInit
+  @ViewChild('videoPlayer', {static: true}) target?: ElementRef
 
   constructor(
     private route: ActivatedRoute
